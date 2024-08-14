807efabeb4a086c23cb6765f32551ce8e9522e8d
Aditya Singh <aditya.singh@binmile.com>
2024-08-14 17:17:36 +0530

    solved the merge havoc

--- src/Screens/ScreenHome/ScreenHome.tsx
+++ src/Screens/ScreenHome/ScreenHome.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react';
-import { View } from 'react-native';
+import { StatusBar, View } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { fetchDataRequest } from './Redux/Slices/HomeSlice';
-import Header from './Components/ComponentHeader';
-import CardList from './Components/ComponentCardVideosList';
+import CombinedCardList from './Components/CombinedCardList';
 import { styles } from './StylesHome';
-import HardcodedCardList from './Components/ComponentCardCourseList';
+import Header from './Components/ComponentHeader';
+import { VideosScreenProps } from '../../utils/interfaces/types';
 
-const HomeScreen: React.FC = () => {
+const HomeScreen: React.FC<VideosScreenProps> = ({navigation, route}) => {
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -16,10 +16,10 @@ const HomeScreen: React.FC = () => {
 
   return (
     <View style={styles.container}>
+     
       <StatusBar translucent backgroundColor="transparent" barStyle="light-content" />
       <Header/>
-      <CombinedCardList />
-      <NavbarComponent/>
+      <CombinedCardList />  
     </View>
   );
 };
--- src/Screens/utils/type.ts
+++ src/Screens/utils/type.ts
@@ -1,17 +1,16 @@
 export interface DownloadedVideo {
-    contentId: number;
-    source: string;
-    artistName: string;
-    songName: string;
-    downloadDate: Date;
-    posterImage: string;
-    isAudio: boolean;
-  }  
-  export interface DownloadProgress {
-    contentId: number;
-    source: string;
-    artistName: string;
-    songName: string;
-    progressValue: string;
-  }
-  
+  contentId: number;
+  source: string;
+  artistName: string;
+  songName: string;
+  downloadDate: Date;
+  posterImage: string;
+  isAudio: boolean;
+}  
+export interface DownloadProgress {
+  contentId: number;
+  source: string;
+  artistName: string;
+  songName: string;
+  progressValue: string;
+}
