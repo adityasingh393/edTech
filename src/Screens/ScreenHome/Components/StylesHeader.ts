b5e6ecfe10cd6e64b8bd6427f9c4dcb504920de4
Aakansha Tyagi <aakansha.tyagi@binmile.com>
2024-08-20 18:15:12 +0530

    Header styling and Added navigation in Navbar Component

--- src/Screens/ScreenHome/Components/StylesHeader.ts
+++ src/Screens/ScreenHome/Components/StylesHeader.ts
@@ -1,24 +1,40 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
+import colors from '../../../utils/Theme';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "../../../utils/Dimensions";
+
 const styles = StyleSheet.create({
-    headerContainer: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      padding: 10,
-      backgroundColor: '#6177EE',
-      height:100,
-    },
-    headerText: {
-      fontSize: 28,
-      color:'#fff',
-      fontWeight:'700',
-      top:15,
-    },
-    
-    UserProfile:{
-      top:15,
-      right:10,
-      
-    }
-  });
-  export default styles;
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: wp('2.5%'), 
+    backgroundColor: colors.primary,
+    height: hp('12.5%'), 
+  },
+  headerText: {
+    fontSize: wp('7%'), 
+    color: colors.white,
+    fontWeight: '700',
+    marginTop: hp('2%'), 
+    marginLeft: wp('15%'), 
+  },
+  UserProfile: {
+    marginTop: hp('2.5%'),
+    right: wp('45%'),
+  },
+  logoutButton: {
+    paddingVertical: hp('1.5%'), 
+    paddingHorizontal: wp('5%'), 
+    backgroundColor: colors.white,
+    borderRadius: wp('9.5%'), 
+    alignItems: 'center',
+    marginTop: hp('3%'),
+  },
+  logoutButtonText: {
+    color: colors.primary,
+    fontSize: wp('4%'),
+    fontWeight: 'bold',
+  },
+});
+
+export default styles;
