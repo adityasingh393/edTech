55fd873f36bb694acaf2796c6f7a1c56b84e4a63
Aditya Singh <aditya.singh@binmile.com>
2024-08-30 17:47:04 +0530

    added the download icno in mediaplayer

--- src/Screens/ScreenVideoPlayer/ScreenVideoPlayer.tsx
+++ src/Screens/ScreenVideoPlayer/ScreenVideoPlayer.tsx
@@ -11,34 +11,18 @@ import NavbarComponent from '../../CommonComponents/NavbarComponent';
 
 const VideoPlayer: React.FC<VideosScreenProps> = ({ navigation, route }) => {
   const { videoUri, title, contentId, thumbnailUrl, description } = route.params;
-  const [isDownloading, setIsDownloading] = useState<boolean>(false);
 
   const handleDownloadPdf = async () => {
     await downloadPdf();
   };
-
-  const handleDownloadVideo = async () => {
-    setIsDownloading(true);
-    downloadVideo(contentId, videoUri, title, thumbnailUrl, () => {
-      setIsDownloading(false);
-      Alert.alert("Success", "Video downloaded successfully.");
-    },
-    ()=>{
-      setIsDownloading(false);
-      Alert.alert("video has already been downloaded")
-    }
-).catch(() => {
-      
-      setIsDownloading(false);
-      Alert.alert("Error", "Failed to download video.");
-    });
-  };
-
   return (
     <View style={styles.container}>
       <NavbarComponent/>
       <View style={styles.mediaPlayerContainer}>
-        <MediaPlayer videoUri={videoUri} />
+        <MediaPlayer videoUri={videoUri} canDownload={true}
+          contentId={contentId}  
+          title={title}
+          thumbnailUrl={thumbnailUrl} />
       </View>
       <View style={styles.detailsContainer}>
         <View style={styles.downloadContainer}>
@@ -54,17 +38,7 @@ const VideoPlayer: React.FC<VideosScreenProps> = ({ navigation, route }) => {
               </View>
             </TouchableOpacity>
           </LinearGradient>
-          <LinearGradient
-            colors={['#C72FF8', '#6177EE', '#6177EE']}
-            start={{ x: 0.9, y: -0.3 }}
-            style={styles.downloadButton}
-          >
-            <TouchableOpacity onPress={handleDownloadVideo} disabled={isDownloading}>
-              <Text style={styles.downloadText}>
-                {isDownloading ? "Downloading..." : "Download Video"}
-              </Text>
-            </TouchableOpacity>
-          </LinearGradient>
+        
         </View>
         <Text style={styles.descriptionTitle}>About Course</Text>
         <Text style={styles.descriptionText}>{description}</Text>
