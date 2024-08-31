205614baf88d3953f7a5c295aeb44b83c5ee3a9d
Abhishek Singh <singh.abhishek@binmile.com>
2024-09-01 00:20:50 +0530

    added offline alert functionality

--- src/Screens/ScreenHome/Components/CombinedCardList.tsx
+++ src/Screens/ScreenHome/Components/CombinedCardList.tsx
@@ -1,34 +1,54 @@
-
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, ActivityIndicator, Animated, RefreshControl } from 'react-native';
+import { View, FlatList, ActivityIndicator, Animated, RefreshControl } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../../../Redux/store';
-import { fetchDataRequest } from '../Redux/Slices/HomeSlice'; 
+import { fetchDataRequest } from '../Redux/Slices/HomeSlice';
 import { cardsData } from '../Utils/Constants';
 import { styles } from './StylsCardCourse';
-
+import NetInfo from '@react-native-community/netinfo';
 import CardCourse from './ComponentCardCourseList';
 import { ExpandingDot } from 'react-native-animated-pagination-dots';
+import OfflineAlertBottomSheet from '../../../CommonComponents/OfflineAlertBottomSheet';
+import { AppStackParamList } from '../../../utils/interfaces/types';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { useNavigation } from '@react-navigation/native';
+
+type NavigationProp = StackNavigationProp<AppStackParamList>;
 
 const CombinedCardList: React.FC = () => {
+  const navigation = useNavigation<NavigationProp>();
   const dispatch = useDispatch();
-  const {
-    data: fetchedData,
-    loading,
-    error,
-  } = useSelector((state: RootState) => state.home);
+  const { data: fetchedData, loading } = useSelector((state: RootState) => state.home);
 
   const scrollX = React.useRef(new Animated.Value(0)).current;
-  const [refreshing, setRefreshing] = useState(loading);
+  const [refreshing, setRefreshing] = useState(false);
+  const [isOffline, setIsOffline] = useState(false);
+  const [showOfflineAlert, setShowOfflineAlert] = useState(false); 
 
   useEffect(() => {
     dispatch(fetchDataRequest());
   }, [dispatch]);
 
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener(state => {
+      if (state.isConnected !== null && !state.isConnected !== isOffline) {
+        setIsOffline(!state.isConnected);
+        setShowOfflineAlert(!state.isConnected); 
+      }
+    });
+
+    return () => unsubscribe();
+  }, [isOffline]);
+
   const onRefresh = () => {
     setRefreshing(true);
-    dispatch(fetchDataRequest())
-     setRefreshing(false);
+    dispatch(fetchDataRequest());
+    if (isOffline) setShowOfflineAlert(true); 
+    setRefreshing(false);
+  };
+
+  const handleNavigateToDownloads = () => {
+    navigation.navigate('Downloads');
   };
 
   const isLoading = loading && !refreshing;
@@ -40,7 +60,13 @@ const CombinedCardList: React.FC = () => {
           <ActivityIndicator size="large" color="#0000ff" />
         </View>
       )}
-      {error && !refreshing && <Text style={styles.error}>Error: {error}</Text>}
+      {!isLoading && !refreshing && showOfflineAlert && (
+        <OfflineAlertBottomSheet
+          isVisible={showOfflineAlert}
+          onDismiss={() => setShowOfflineAlert(false)}
+          onNavigateToDownloads={handleNavigateToDownloads}
+        />
+      )}
       {!isLoading && (
         <FlatList
           data={cardsData.map((item, index) => ({
@@ -53,9 +79,7 @@ const CombinedCardList: React.FC = () => {
           showsHorizontalScrollIndicator={false}
           onScroll={Animated.event(
             [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-            {
-              useNativeDriver: false,
-            }
+            { useNativeDriver: false }
           )}
           pagingEnabled
           snapToAlignment="center"
@@ -69,11 +93,12 @@ const CombinedCardList: React.FC = () => {
           }
         />
       )}
-       <ExpandingDot
-data={cardsData.map((item, index) => ({
-  ...item,
-  fetchedItems: fetchedData.slice(index * 5, index * 5 + 5),
-}))}        scrollX={scrollX}
+      <ExpandingDot
+        data={cardsData.map((item, index) => ({
+          ...item,
+          fetchedItems: fetchedData.slice(index * 5, index * 5 + 5),
+        }))}
+        scrollX={scrollX}
         inActiveDotOpacity={0.6}
         dotStyle={styles.dotStyle}
         containerStyle={styles.dotContainer}
